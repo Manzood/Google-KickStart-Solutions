5c73618525fbc2abf60b67a721cacaa0f9f569cf
Manzood <manzood.naqvi@gmail.com>
2022-01-09 01:31:16 +0530

    Began an attempt at D

--- 2021/Round-H/D/D.cpp
+++ 2021/Round-H/D/D.cpp
@@ -1,18 +1,31 @@
 #include "bits/stdc++.h"
-
 #ifdef local
     #include "custom/debugger.h"
 #else
     #define debug(x) 42;
 #endif
-
 using namespace std;
 #define int long long
+
+void testcase() {
+    int n, q;
+    scanf("%lld%lld", &n, &q);
+    int k;
+    scanf("%lld", &k);
+    vector <int> parent(n, -1), a(n), b(n);
+    a[0] = b[0] = -1;
+    for (int i = 1; i < n; i++) {
+        scanf("%lld%lld%lld", &parent[i], &a[i], &b[i]);
+    }
+    // recursive function to find the probability every time??
+}
 
 int32_t main () {
     int t;
     cin >> t;
-    while (t--) {
-
+    for (int tt = 1; tt <= t; tt++) {
+        printf("Case #%lld: ", tt);
+        testcase();
+        printf("\n");
     }
 }
